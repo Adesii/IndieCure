ee089a853878b66262a88744a56b2c21a1f52321
Adesi <Kopietz123@gmail.com>
2023-10-01 17:31:00 +0200

    Proper shadows

--- Scripts/Characters/AccurateShadow.gd
+++ Scripts/Characters/AccurateShadow.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var sprite : Sprite2D = get_parent()
 
 var dummysprite2d : Sprite2D
+var copy_transform : RemoteTransform2D
 
 func _ready():
 	dummysprite2d = Sprite2D.new()
@@ -13,13 +14,21 @@ func _ready():
 	dummysprite2d.hframes = sprite.hframes
 	dummysprite2d.vframes = sprite.vframes
 	dummysprite2d.offset = Vector2(sprite.offset.x, -sprite.offset.y)
-	dummysprite2d.skew = 0.1
-	dummysprite2d.modulate = Color(0,0,0,0.2)
-	sprite.add_child.call_deferred(dummysprite2d)
+	dummysprite2d.position = sprite.position
+	dummysprite2d.show_behind_parent = true
+	
+	Global.shadow_canvas_group.add_child.call_deferred(dummysprite2d)
+	
 
 func _physics_process(delta):
-		
-	
+	if dummysprite2d.is_inside_tree() and copy_transform == null :
+		copy_transform = RemoteTransform2D.new()
+		#copy_transform.update_scale = false
+		#copy_transform.update_rotation = false
+		copy_transform.remote_path = dummysprite2d.get_path()
+		sprite.add_child(copy_transform)
+	if copy_transform != null :
+		copy_transform.position = sprite.position
 	dummysprite2d.frame = sprite.frame
 	dummysprite2d.flip_h = sprite.flip_h
 
--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -5,16 +5,21 @@ var current_character : IndieCharacter = load("res://Characters/Anny/Anny.tres")
 
 var player : CharacterBody2D
 
+var shadow_canvas_group : CanvasGroup
+
 var current_scene = null
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
+	shadow_canvas_group = load("res://Scripts/Global/ShadowGroup.tscn").instantiate()
 
 	if get_tree().current_scene is Control:
 		return
+	
 	current_scene = get_tree().current_scene
 	var player_scene = current_character.character_scene.instantiate();
 	player = player_scene
+	current_scene.add_child(shadow_canvas_group)
 	current_scene.add_child(player_scene)
 
 func _input(event):
