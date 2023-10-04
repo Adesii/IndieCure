9f0ba2ae31753968c3f5fda27fafae1689e1df6a
Adesi <Kopietz123@gmail.com>
2023-10-04 05:12:55 +0200

    Combine Characters into single scene

--- Scripts/Characters/CharacterResource.gd
+++ Scripts/Characters/CharacterResource.gd
@@ -1,6 +1,6 @@
 extends Resource
 class_name IndieCharacter
 
-@export var character_scene : PackedScene
+@export var character_animations : SpriteFrames
 @export var icon : Texture2D
 
--- Scripts/Characters/PlayerSprite.gd
+++ Scripts/Characters/PlayerSprite.gd
@@ -1,28 +1,35 @@
 extends Node2D
 
-@export var playersprite : Node2D
+## Optional, if you want to use a different sprite for the animation
+@export var animated_sprite : Node2D
+
+@export var character_sprite : CharacterBody2D
 
 @export var fliptimelimit = 400
 
 var lastfliptime = 0
 
 func _ready():
-	if self.has_method("play"):
-		self.call("play")
+	#fallback if not assigned
+	if animated_sprite == null:
+		animated_sprite = self
+
+	if animated_sprite.has_method("play"):
+		animated_sprite.call("play")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	if playersprite == null:
+	if animated_sprite == null:
 		return
-	if playersprite.velocity.length() > 1:
-		self.set("animation","run")
+	if character_sprite.velocity.length() > 1:
+		animated_sprite.set("animation","run")
 	else:
-		self.set("animation","idle")
+		animated_sprite.set("animation","idle")
 	#add a little delay to the flip so it doesn't flip every frame
-	if playersprite.velocity.x > 0 and lastfliptime + fliptimelimit < Time.get_ticks_msec():
-		self.flip_h = false
+	if character_sprite.velocity.x > 0 and lastfliptime + fliptimelimit < Time.get_ticks_msec():
+		animated_sprite.flip_h = false
 		lastfliptime = Time.get_ticks_msec()
-	elif playersprite.velocity.x < 0 and lastfliptime + fliptimelimit < Time.get_ticks_msec():
-		self.flip_h = true
+	elif character_sprite.velocity.x < 0 and lastfliptime + fliptimelimit < Time.get_ticks_msec():
+		animated_sprite.flip_h = true
 		lastfliptime = Time.get_ticks_msec()
 
