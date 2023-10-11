ed0c3bee0312084297d6589347328a9d6615c198
Adesi <Kopietz123@gmail.com>
2023-10-11 18:07:06 +0200

    New Bella Animations

--- gif2spritesheet.bat
+++ gif2spritesheet.bat
@@ -3,6 +3,3 @@ REM Converts an animated gif to a spritesheet using ImageMagick
 set file_name=%~n1
 magick montage %1 -tile x1 -geometry "1x1+0+0<" -alpha On -background "rgba(0, 0, 0, 0.0)" -quality 100 %file_name%.png
 echo Converted %1 to %file_name%.png
-
-REM wait for user input
-pause
