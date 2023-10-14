840558e2f7486b0b0996ab173efd473cc21ffa54
Adesi <Kopietz123@gmail.com>
2023-10-14 19:49:46 +0200

    1000 enemies

--- Scenes/Worlds/StageOne/Scripts/StageOne.gd
+++ Scenes/Worlds/StageOne/Scripts/StageOne.gd
@@ -3,7 +3,7 @@ extends WavesController
 
 func _ready():
 	super._add_wave(0, "non-existing-spawner-1")
-	super._add_wave(10, "TofSpawner")
+	super._add_wave(1, "TofSpawner")
 	super._add_wave(20, "non-existing-spawner-2")
 
 	super._ready()
