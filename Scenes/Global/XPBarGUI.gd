47cc35b6d250e2e502da47fc0abaa1b41fc2fe30
Adesi <Kopietz123@gmail.com>
2024-01-17 03:19:56 +0100

    Rework XP_bar to be more accurate

--- Scenes/Global/XPBarGUI.gd
+++ Scenes/Global/XPBarGUI.gd
@@ -7,42 +7,51 @@ extends Control
 
 var level : int = 1
 
+var level_req : float = 100
+
+var accounted_xp : float = 0
+var current_xp : float = 0
+
 var transition : Tween
+
+var xpattribute : Attribute
 
 
 
 
 func _ready():
-	var xpAttribute = Stat.Get_Attribute(Global.player, "xp")
-	xpAttribute.max_value = round((pow(level, 1.8) + level * 4)*10)
-	xpAttribute.value_changed.connect(_on_xp_changed)
-	xpAttribute.value_maxed.connect(_on_xp_maxed)
+	var xxpAttribute = Stat.Get_Attribute(Global.player, "xp")
+	xpattribute = xxpAttribute
+	xpattribute.max_value = round((pow(level, 1.8) + level * 4)*10)
+	level_req = round((pow(level, 1.8) + level * 4)*10)
+	#xpattribute.value_changed.connect(_on_xp_changed)
+	#xpAttribute.value_maxed.connect(_on_xp_maxed)
 	xp_bar.scale.x = 0
 
-func _on_xp_changed(attr,_info):
-	var xpAttribute = attr
-	lvl_text.text ="lvl: "+ str(level) + "\nXP: " + str(xpAttribute.current_value) + " / "+ str(xpAttribute.max_value)
+func _process(delta: float) -> void:
+	current_xp = xpattribute.get_value()
+	if get_tree().paused:
+		return
+	lvl_text.text ="lvl: "+ str(level) + "\nXP: " + str(current_xp) + " / "+ str(xpattribute.max_value)
+	accounted_xp = lerp(accounted_xp,current_xp,10*delta)
+	if current_xp-accounted_xp < 0.1:
+		accounted_xp = current_xp
 
-	#TODO: figure out how to do this correctly so it actually wraps around instead of snapping to the new value when a level up happens
-	var curvalclamped = clamp(xpAttribute.get_value_scaled(), 0, 1)
-	if transition != null and transition.is_running():
-		await transition.finished
+	#print("accounted xp: "+str(accounted_xp)+" current xp: "+str(current_xp)+"\n")
+	#print("level req: "+str(level_req)+"\n")
 	
-	transition = create_tween()
+	if roundf(accounted_xp) >= level_req:
+		_on_xp_maxed()
+
 	
-	if curvalclamped < xp_bar.scale.x:
-		transition.tween_property(xp_bar,"scale",Vector2(1,1),0.2)
-		transition.tween_property(xp_bar,"scale",Vector2(0,1),0)
-	transition.tween_property(xp_bar,"scale",Vector2(curvalclamped,1),0.2)
-	transition.play()
-	#xp_bar.scale.x = curvalclamped
+	xp_bar.scale.x = accounted_xp / level_req
 
-	#print("xp changed to: "+str(xpAttribute.current_value)+" scaled: "+str(xpAttribute.get_value_scaled())+" clamped: "+str(curvalclamped)+"\n")
-
-func _on_xp_maxed(attr,_info):
-	var xpAttribute = attr
-	xpAttribute.current_value -= xpAttribute.max_value
+func _on_xp_maxed():
 	level += 1
-	xpAttribute.max_value = round((pow(level, 2.3) + level * 4) * 10)
-	xp_bar.scale.x = 0
-	Global.open_pause_panel(to_open_ui_panel.instantiate())
+	var xpAttribute = Stat.Get_Attribute(Global.player, "xp")
+	accounted_xp = 0
+	xpAttribute.current_value -= level_req
+	level_req = round((pow(level, 2.3) + level * 4) * 10)
+	xpAttribute.max_value = level_req
+	var panel = to_open_ui_panel.instantiate()
+	Global.open_pause_panel(panel)
