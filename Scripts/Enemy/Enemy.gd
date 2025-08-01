cea1f382f1f6361f9339f822a8534c38a276f0e9
Adesi <Kopietz123@gmail.com>
2025-08-01 18:19:23 +0200

    Fix Damage Numbers not showing correct value

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -17,14 +17,16 @@ var damage_frames: float = 0.0
 
 
 func on_damaged(attr: Attribute, info, change_value):
-	if change_value == attr.current_value or change_value < 0:
+	#calculate the difference between current and change value (change value can be negative)
+	var damage_dealt = attr.current_value - change_value
+	if change_value == attr.current_value:
 		return
 	# if enemy took damage display damage numbers
 	if attr.current_value - change_value < 0:
 		return
+	DamageNumbers.add_damage_number(global_position, max(1, damage_dealt), Color(1, 1, 1, 1), 0.8)
 	if damage_frames <= 0:
 		damage_frames = 10
-	DamageNumbers.add_damage_number(global_position, attr.current_value - change_value, Color(1, 1, 1, 1), 0.8)
 
 func _init() -> void:
 	health.value_changing.connect(on_damaged)
